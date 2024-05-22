fb9d794cf2635186bdeba67bdaaf8520ef48601b
ThiagoSausmikat <tsmoura.93@gmail.com>
2024-05-21 22:35:16 -0300

    alteração

--- ecommerce/src/main/java/org/br/serratec/ecommerce/dtos/ProdutoDTO.java
+++ ecommerce/src/main/java/org/br/serratec/ecommerce/dtos/ProdutoDTO.java
@@ -2,10 +2,14 @@ package org.br.serratec.ecommerce.dtos;
 
 import java.time.LocalDateTime;
 
+import org.br.serratec.ecommerce.entities.Categoria;
+
 public record ProdutoDTO(
         String nomeDescricao,
         Integer qtdEstoque,
         LocalDateTime dataCadastro,
         Double valorUnitario,
         Categoria categoria)
-{}
+{
+	
+}
