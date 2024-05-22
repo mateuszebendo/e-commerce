b979d310611eb74742ab2231558524aa9f77f9ad
Foncka <matheus_fonseca237@hotmail.com>
2024-05-21 22:45:18 -0300

    DTO

--- ecommerce/src/main/java/org/br/serratec/ecommerce/dtos/ProdutoDTO.java
+++ ecommerce/src/main/java/org/br/serratec/ecommerce/dtos/ProdutoDTO.java
@@ -2,6 +2,8 @@ package org.br.serratec.ecommerce.dtos;
 
 import java.time.LocalDateTime;
 
+import org.br.serratec.ecommerce.entities.Categoria;
+
 public record ProdutoDTO(
         String nomeDescricao,
         Integer qtdEstoque,
